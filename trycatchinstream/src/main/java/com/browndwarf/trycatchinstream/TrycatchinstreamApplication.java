7fd17350f91420ddc8b4401a2e544586cff07035
first-browndwarf <teideone@outlook.com>
2022-06-30 02:02:28 +0900

    Project Initialize

---
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/TrycatchinstreamApplication.java
@@ -0,0 +1,13 @@
+package com.browndwarf.trycatchinstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrycatchinstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrycatchinstreamApplication.class, args);
+	}
+
+}
