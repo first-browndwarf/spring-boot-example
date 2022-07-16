17a8fd8e3552326e516d48c320e5ee1e8b7569f5
first-browndwarf <99606916+first-browndwarf@users.noreply.github.com>
2022-07-16 10:43:15 +0900

    Update FunctionWithThrows.java
    Define New Functional Interface

--- trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/common/custominterface/FunctionWithThrows.java
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/common/custominterface/FunctionWithThrows.java
@@ -1,5 +1,6 @@
 package com.browndwarf.trycatchinstream.common.custominterface;
 
-public interface FunctionWithThrows {
-
+@FunctionalInterface
+public interface FunctionWithThrows<T, R> {
+    R apply(T t) throws Exception;
 }
