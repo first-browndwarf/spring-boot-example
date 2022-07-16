3f6d7b7f6b64db77277e1f337b97f4b2b8f560a9
first-browndwarf <99606916+first-browndwarf@users.noreply.github.com>
2022-07-16 10:54:24 +0900

    Update Member.java
    Define Member Class

--- trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/model/Member.java
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/model/Member.java
@@ -1,5 +1,27 @@
 package com.browndwarf.trycatchinstream.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Member {
 
+    @JsonProperty("id")
+    private long    id;
+
+    @JsonProperty("name")
+    private String  name;
+
+    @JsonProperty("age")
+    private int age;
+
+    @JsonProperty("group")
+    private String group;
+
+    @JsonProperty("groupInfo")
+    private String groupInfo;
 }
