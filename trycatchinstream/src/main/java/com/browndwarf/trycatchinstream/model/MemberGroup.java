3a7604584036230a1546e1f3dbc3e56c30d83463
first-browndwarf <99606916+first-browndwarf@users.noreply.github.com>
2022-07-16 10:54:54 +0900

    Update MemberGroup.java
    Define MemberGroup class

--- trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/model/MemberGroup.java
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/model/MemberGroup.java
@@ -1,5 +1,22 @@
 package com.browndwarf.trycatchinstream.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class MemberGroup {
+
+    @JsonProperty("name")
+    String  name;
+
+    @JsonProperty("category")
+    String  category;
+
+    @JsonProperty("count")
+    int  memberCount;
 
 }
