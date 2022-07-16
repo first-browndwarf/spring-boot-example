b22a00f4672ef982dd0047910f521a7b03004dcb
first-browndwarf <99606916+first-browndwarf@users.noreply.github.com>
2022-07-16 10:53:33 +0900

    Update CustomFunctionalInterfaces.java
    Class to implement Functional Interface

--- trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/common/util/CustomFunctionalInterfaces.java
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/common/util/CustomFunctionalInterfaces.java
@@ -1,5 +1,30 @@
 package com.browndwarf.trycatchinstream.common.util;
 
-public class CustomFunctionalInterfaces {
+import com.browndwarf.trycatchinstream.common.custominterface.FunctionWithThrows;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.Function;
+
+@Slf4j
+public class CustomFunctionalInterfaces {
+    public static <T,R> Function<T,R> functionWithThrows(FunctionWithThrows<T, R> functionWithThrows) {
+        return item->{
+            try{
+                return functionWithThrows.apply(item);
+            }catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    public static <T,R> Function<T,R> functionWithNull(FunctionWithThrows<T, R> functionWithThrows) {
+        return item->{
+            try{
+                return functionWithThrows.apply(item);
+            }catch(Exception e) {
+                log.error(">>> Occur an exception : {} (Message = {})", e.toString(), e.getMessage());
+                return null;
+            }
+        };
+    }
 }
