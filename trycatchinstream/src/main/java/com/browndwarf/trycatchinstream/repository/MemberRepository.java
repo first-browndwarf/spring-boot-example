57f7168301755b6e378190f3585c9c5613e606db
first-browndwarf <99606916+first-browndwarf@users.noreply.github.com>
2022-07-16 10:55:51 +0900

    Update MemberRepository.java
    Fake Repository

--- trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/repository/MemberRepository.java
+++ trycatchinstream/src/main/java/com/browndwarf/trycatchinstream/repository/MemberRepository.java
@@ -1,5 +1,19 @@
 package com.browndwarf.trycatchinstream.repository;
 
+import com.browndwarf.trycatchinstream.model.Member;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class MemberRepository {
 
+    private List<Member>    memberList;
+
+    public List<Member> findAllByGroup(String groupName) {
+        return memberList.stream()
+                .filter(p->p.getGroup().equalsIgnoreCase(groupName))
+                .collect(Collectors.toList());
+    }
 }
