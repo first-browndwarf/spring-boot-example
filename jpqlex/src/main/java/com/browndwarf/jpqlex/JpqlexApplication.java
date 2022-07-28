dced84b3db9ad7c37ff0f49bf0c008bc0b07e18f
first-browndwarf <teideone@outlook.com>
2022-07-29 01:08:44 +0900

    Initialize a project for JPQL Example

---
+++ jpqlex/src/main/java/com/browndwarf/jpqlex/JpqlexApplication.java
@@ -0,0 +1,13 @@
+package com.browndwarf.jpqlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpqlexApplication.class, args);
+	}
+
+}
---
+++ jpqlex/src/test/java/com/browndwarf/jpqlex/JpqlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.browndwarf.jpqlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
